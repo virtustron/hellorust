7a1faf28d2cee2b1470750919ccdfe58cba88468
virtustron <virtustron@gmail.com>
2021-10-02 14:05:33 +0300

    Working example project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+
+    let random_number = rand::thread_rng().gen_range(0..10);
+ 
+    println!("Generated number: {}", random_number);
 }
