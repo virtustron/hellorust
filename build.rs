675a32d608c7a9d59a7d6a221fef4dba91bb31c1
virtustron <virtustron@gmail.com>
2021-10-05 23:20:30 +0300

    Need fix to build - could not compile

--- build.rs
+++ build.rs
@@ -27,7 +27,7 @@ fn main() {
         .include("include")
         .flag("-Wno-unused-parameter")
         .define("USE_ZLIB", None);
-    build.compile("foo");
+    build.compile("bindtest");
     
     
     
@@ -53,6 +53,6 @@ fn main() {
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
+        .write_to_file(out_path.join("bindtest.rs"))
         .expect("Couldn't write bindings!");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use rand::Rng;
 
-fn main() {
-    //println!("Hello, world!");
 
+mod bindings {
+    include!(concat!(env!("OUT_DIR"), "/bindtest.rs"));
+}
+
+
+fn main() {
     let random_number = rand::thread_rng().gen_range(0..10);
- 
     println!("Generated number: {}", random_number);
+
+    
+    unsafe {
+        let perimeter = bindings::CalculatePerimeter(1, 2, 3);
+        println!("CalculatePerimeter result: {}", perimeter);
+    }
+
+    unsafe {
+        let sum = bindings::CalculateSum(5, 5);
+        println!("CalculateSum result: {}", sum);
+    }
 }
